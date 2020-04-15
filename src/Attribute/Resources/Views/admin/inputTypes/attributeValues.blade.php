6345cf5be4d92fbb81f2c2be9deb6aad6d6915ba
mehrdad <javadabbasi72@gmail.com>
2020-04-15 22:21:35 +0430

    bug fixed

--- src/Attribute/Resources/Views/admin/inputTypes/attributeValues.blade.php
+++ src/Attribute/Resources/Views/admin/inputTypes/attributeValues.blade.php
@@ -19,13 +19,15 @@
 
 @endphp
 
+
+<div class="row sortable-header">
+    <div class="col-md-4 col-xs-12 ">
+        <label> @lang("$crud->name::panel.name") </label>
+    </div>
+</div>
+
 <div class="cloning sortable">
 
-    <div class="row labels">
-        <div class="col-md-4 col-xs-12 ">
-            <label> @lang("$crud->name::panel.name") </label>
-        </div>
-    </div>
 
     @foreach ($attributeValues as $attributeValue)
         <div class="item">
@@ -33,7 +35,7 @@
                 <input type="hidden" name-template="values[xxx][id]" name="values[{{$loop->index}}][id]"
                        value="{{$attributeValue->id}}" class="form-control @error($field->name) is-invalid @enderror">
                 <div class="col-md-12 col-xs-12 form-group">
-                    <input type="text" id="value-label-{{$loop->index}}" id-template="value-label-xxx" required
+                    <input type="text" id="values-{{$loop->index}}-value" id-template="values-xxx-value" required
                            name-template="values[xxx][value]" name="values[{{$loop->index}}][value]"
                            value="{{ $attributeValue->value }}" class="form-control @error(" values[{{$loop->index}}][value]")
                             is-invalid @enderror">
@@ -54,106 +56,110 @@
 @push('firstScripts')
     <script>
         var $cloning,
-            $item,
-            templateHtml,
-            dataId;
+                $item,
+                templateHtml,
+                dataId;
 
-        function runCloning(){
-            $cloning = $('.cloning');
-            $item = $cloning.find('.item');
+            function runCloning(){
+                $cloning = $('.cloning');
+                $item = $cloning.find('.item');
 
-            $(".cloning").after(`<a class="plus btn btn-info">+</a>`);
+                $(".cloning").after(`<a class="plus btn"><i class="fas fa-plus"></i></a>`);
 
-            dataId = $item.length;
+                dataId = $item.length - 1;
 
-            $item.each(function(index ){
 
-                templateHtml = $(this).html();
 
-                $(this).remove();
+                $item.each(function(index ){
+
+                    templateHtml = $(this).html();
+
+                    $(this).remove();
+                    var newItem = $('<div class="item"></div>');
+
+                    $(newItem).append(`<a class="remove-item btn"><i class="fas fa-times"></i></a> ${templateHtml} `);
+
+                    $(newItem).find('[name]').each(function(){
+
+                        var nameTemplate = $(this).attr('name-template');
+                        $(this).removeAttr('name-template');
+                        newName = replaceAll(nameTemplate, 'xxx', dataId);
+                        $(this).attr('name', newName );
+
+
+                    });
+
+
+                    $(newItem).find('[id]').each(function(){
+
+                        var idTemplate = $(this).attr('id-template');
+                        $(this).removeAttr('id-template');
+                        idName = replaceAll(idTemplate, 'xxx', dataId);
+                        $(this).attr('id', idName );
+
+                    });
+
+                    $('.cloning').append(newItem);
+                    dataId++;
+                });
+            }
+
+
+            runCloning();
+
+            function addRow(){
+
                 var newItem = $('<div class="item"></div>');
-
                 $(newItem).append(`<a class="remove-item btn"><i class="fas fa-times"></i></a> ${templateHtml} `);
 
                 $(newItem).find('[name]').each(function(){
-
                     var nameTemplate = $(this).attr('name-template');
                     $(this).removeAttr('name-template');
                     newName = replaceAll(nameTemplate, 'xxx', dataId);
                     $(this).attr('name', newName );
+                    $(this).val('');
+                });
 
 
-                var newHtml = templateHtml.replace("name-template", "name");
-                newHtml = newHtml.replace("xxx", index);
 
-                $(".cloning").append(`<div class="item"> <a class="remove-item btn btn-danger">x</a> ${newHtml} </div>`);
-
-                $(this).append(`<a class="remove-item btn btn-danger">x</a>`)
-
+                $(newItem).find('[id]').each(function(){
                     var idTemplate = $(this).attr('id-template');
                     $(this).removeAttr('id-template');
                     idName = replaceAll(idTemplate, 'xxx', dataId);
                     $(this).attr('id', idName );
-
                 });
+
+
+
+
 
                 $('.cloning').append(newItem);
                 dataId++;
+            }
+
+
+            $('.cloning').on( "click", 'a.remove-item' ,function (e) {
+                e.preventDefault();
+                $(this).parents('.item').remove();
             });
+
+            $('body').on( "click", 'a.plus', function () {
+                addRow();
+                $(".select2").select2();
+
+            });
+
+
+            $(function () {
+                $(".sortable").sortable();
+                $(".sortable").disableSelection();
+            });
+
+
+
+        function replaceAll(str, find, replace) {
+            return str.replace(new RegExp(find, 'g'), replace);
         }
-
-
-        runCloning();
-
-        function addRow(){
-
-            var newItem = $('<div class="item"></div>');
-            $(newItem).append(`<a class="remove-item btn"><i class="fas fa-times"></i></a> ${templateHtml} `);
-
-            $(newItem).find('[name]').each(function(){
-                var nameTemplate = $(this).attr('name-template');
-                $(this).removeAttr('name-template');
-                newName = replaceAll(nameTemplate, 'xxx', dataId);
-                $(this).attr('name', newName );
-                $(this).val('');
-            });
-
-
-
-            $(newItem).find('[id]').each(function(){
-                var idTemplate = $(this).attr('id-template');
-                $(this).removeAttr('id-template');
-                idName = replaceAll(idTemplate, 'xxx', dataId);
-                $(this).attr('id', idName );
-            });
-
-
-
-
-
-            $('.cloning').append(newItem);
-            dataId++;
-
-        }
-
-
-        $('.cloning').on( "click", 'a.remove-item' ,function (e) {
-            e.preventDefault();
-            $(this).parents('.item').remove();
-        });
-
-        $('body').on( "click", 'a.plus', function () {
-            addRow();
-            $(".select2").select2();
-
-        });
-
-
-        $(function () {
-            $(".sortable").sortable();
-            $(".sortable").disableSelection();
-        });
-
     </script>
 @endpush
 
