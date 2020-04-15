8736035b295e122fc9c55cc71d4525b03593956e
mehrdad <javadabbasi72@gmail.com>
2020-04-15 15:53:28 +0430

    attribiute develop

--- src/Attribute/Resources/Views/admin/inputTypes/attributeValues.blade.php
+++ src/Attribute/Resources/Views/admin/inputTypes/attributeValues.blade.php
@@ -19,50 +19,15 @@
 
 @endphp
 
-<style>
-    .ui-sortable .item{
-        padding-left: 20px;
-        position: relative;
-    }
-    .item{
-        padding-right: 40px;
-    }
-    .ui-sortable .labels{
-        padding-left: 20px;
-        position: relative;
+<div class="row sortable-header">
+    <div class="col-12">
+        <label> name </label>
+    </div>
+ </div>
 
-    }
-    .header{
-        padding-right: 40px;
-    }
-    .remove-item{
-        position: absolute;
-        right: 0;
-        bottom: 1rem;
-        cursor: pointer;
-    }
-    .ui-sortable .item::before{
-        content: '';
-        width: 20px;
-        height: 20px;
-        position: absolute;
-        left: 0;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-        background-image: url("data:image/svg+xml,%0A%3Csvg height='40' viewBox='-149 0 512 512.10667' width='40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m170.71875 256.105469c0 35.347656-28.652344 64-64 64-35.34375 0-64-28.652344-64-64 0-35.34375 28.65625-64 64-64 35.347656 0 64 28.65625 64 64zm0 0'/%3E%3Cpath d='m192.054688 362.773438h-170.667969c-11.753907 0-21.3320315 9.578124-21.3320315 21.332031 0 4.03125 1.0664065 7.832031 3.2851565 11.5625.382812.640625.808594 1.261719 1.277344 1.835937l86.570312 108.03125c4.269531 4.246094 9.773438 6.570313 15.53125 6.570313 5.761719 0 11.265625-2.324219 16.769531-7.914063l85.332031-106.667968c.46875-.574219.898438-1.214844 1.28125-1.832032 2.21875-3.753906 3.285157-7.554687 3.285157-11.585937 0-11.753907-9.578125-21.332031-21.332031-21.332031zm0 0'/%3E%3Cpath d='m21.386719 149.441406h170.667969c11.753906 0 21.332031-9.578125 21.332031-21.335937 0-4.03125-1.066407-7.828125-3.285157-11.5625-.382812-.640625-.8125-1.257813-1.28125-1.832031l-86.570312-108.035157c-8.53125-8.445312-21.289062-9.8125-32.296875 1.367188l-85.335937 106.667969c-.46875.574218-.894532 1.214843-1.277344 1.832031-2.21875 3.734375-3.2851565 7.53125-3.2851565 11.5625 0 11.757812 9.5781245 21.335937 21.3320315 21.335937zm0 0'/%3E%3C/svg%3E");
-        bottom: 1.5rem;
-        cursor: move;
-    }
-</style>
 
 <div class="cloning sortable">
 
-        <div class="row labels">
-            <div class="col-12">
-                <label> name </label>
-            </div>
-         </div>
 
 
 
@@ -76,7 +41,9 @@
             <div class="row">
                 <div class="col-12 form-group">
                     <input type="text"
+                           id-template="values-xxx"
                            id="values-{{$loop->index}}"
+                           required
                            name-template="values[xxx][value]"
                            name="values[{{$loop->index}}][value]"
                            value="{{$attributeValue->value}}"
@@ -89,10 +56,9 @@
 
 
 
-@push('scripts')
-    <script>
-
-        var $cloning,
+@push('firstScripts')
+<script>
+    var $cloning,
             $item,
             templateHtml,
             dataId;
@@ -101,23 +67,43 @@
             $cloning = $('.cloning');
             $item = $cloning.find('.item');
 
-            $(".cloning").after(`<a class="plus btn btn-info">+</a>`);
+            $(".cloning").after(`<a class="plus btn"><i class="fas fa-plus"></i></a>`);
 
-            dataId = $item.length;
+            dataId = $item.length - 1;
+
+
 
             $item.each(function(index ){
 
                 templateHtml = $(this).html();
 
                 $(this).remove();
+                var newItem = $('<div class="item"></div>');
 
-                var newHtml = templateHtml.replace("name-template", "name");
-                newHtml = newHtml.replace("xxx", index);
+                $(newItem).append(`<a class="remove-item btn"><i class="fas fa-times"></i></a> ${templateHtml} `);
 
-                $(".cloning").append(`<div class="item"> <a class="remove-item btn btn-danger">x</a> ${newHtml} </div>`);
+                $(newItem).find('[name]').each(function(){
 
-                $(this).append(`<a class="remove-item btn btn-danger">x</a>`)
+                    var nameTemplate = $(this).attr('name-template');
+                    $(this).removeAttr('name-template');
+                    newName = replaceAll(nameTemplate, 'xxx', dataId);
+                    $(this).attr('name', newName );
 
+
+                });
+
+
+                $(newItem).find('[id]').each(function(){
+
+                    var idTemplate = $(this).attr('id-template');
+                    $(this).removeAttr('id-template');
+                    idName = replaceAll(idTemplate, 'xxx', dataId);
+                    $(this).attr('id', idName );
+
+                });
+
+                $('.cloning').append(newItem);
+                dataId++;
             });
         }
 
@@ -126,16 +112,32 @@
 
         function addRow(){
 
-            var newHtml = templateHtml.replace("name-template", "name" );
-            newHtml = newHtml.replace("xxx", dataId );
-
             var newItem = $('<div class="item"></div>');
-            $(newItem).append(`<a class="remove-item btn btn-danger">x</a> ${newHtml} `);
-            $(newItem).find('[name]').val('');
-            $(newItem).find('[name]').first().attr('autofocus','true');
+            $(newItem).append(`<a class="remove-item btn"><i class="fas fa-times"></i></a> ${templateHtml} `);
+
+            $(newItem).find('[name]').each(function(){
+                var nameTemplate = $(this).attr('name-template');
+                $(this).removeAttr('name-template');
+                newName = replaceAll(nameTemplate, 'xxx', dataId);
+                $(this).attr('name', newName );
+                $(this).val('');
+            });
+
+
+
+            $(newItem).find('[id]').each(function(){
+                var idTemplate = $(this).attr('id-template');
+                $(this).removeAttr('id-template');
+                idName = replaceAll(idTemplate, 'xxx', dataId);
+                $(this).attr('id', idName );
+            });
+
+
+
+
+
             $('.cloning').append(newItem);
             dataId++;
-
         }
 
 
@@ -146,6 +148,8 @@
 
         $('body').on( "click", 'a.plus', function () {
             addRow();
+            $(".select2").select2();
+
         });
 
 
@@ -154,7 +158,13 @@
             $(".sortable").disableSelection();
         });
 
-    </script>
+
+
+    function replaceAll(str, find, replace) {
+        return str.replace(new RegExp(find, 'g'), replace);
+    }
+</script>
+
 @endpush
 
 
