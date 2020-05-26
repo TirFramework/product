dd65e56fe55b6a056954bac3b73d2e8403af1b04
Tirdad <abbasi.tirdad@gmail.com>
2020-05-26 09:45:29 +0430

    frontend development and debugging

--- src/Product/Support/Price.php
+++ src/Product/Support/Price.php
@@ -9,17 +9,18 @@ use Illuminate\Support\HtmlString;
 class Price
 {
     //TODO: chenge currency system
-    public static $currency = 'تومان';
+    public static $currency = ' تومان';
 
     public static function render($product, $class = 'previous-price')
     {
         if (! $product->hasSpecialPrice()) {
-            return floor($product->price).self::$currency;
+
+            return number_format($product->price).self::$currency;
         }
 
         $price = floor($product->price);
-        $specialPrice = floor($product->special_price).self::$currency;
+        $specialPrice = number_format($product->special_price).self::$currency;
 
-        return new HtmlString("<span>{$specialPrice}</span> <span class='{$class}'>{$price}</span>");
+        return new HtmlString(" <span class='{$class}'>{$price}</span> <span>{$specialPrice}</span>");
     }
 }
