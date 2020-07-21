0142cc3bc446e29348854efe4b3e7772181d873c
Tirdad <abbasi.tirdad@gmail.com>
2020-07-21 16:02:26 +0430

    Blog Development

--- src/Wishlist/Database/Migrations/2018_10_01_224852175056_create_wish_lists_table.php
+++ src/Wishlist/Database/Migrations/2018_10_01_224852175056_create_wish_lists_table.php
@@ -13,7 +13,7 @@ class CreateWishListsTable extends Migration
     public function up()
     {
         Schema::create('wish_lists', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->integer('product_id')->unsigned();
             $table->timestamps();
 
